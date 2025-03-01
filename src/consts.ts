67ac6a321d777a734fc8c7c5cdcbb25898542783
GeauxWeisbeck4 <andrew.weisbeck@gmail.com>
2025-03-01 13:02:00 -0500

    Initial commit

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Andrew Weisbeck Portfolio Website | Full-Stack Developer';
+export const SITE_DESCRIPTION = 'Andrew Weisbeck Full-Stack Developer Portfolio and Resume Website';
---
+++ src/content/projects/developer-portfolio-site.md
@@ -0,0 +1,8 @@
+---
+title: 'Developer Portfolio Site'
+description: 'My developer portfolio site is where you can find everything about myself and my developer career, including projects, blog posts, notes, and more.'
+pubDate: 'Feb 26 2025'
+heroImage: '/blog-placeholder-3.jpg'
+---
+
+Welcome to my developer portfolio site.
